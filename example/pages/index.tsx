62ba6e888a62c8ac57fbd0efac137833bd9c8982
liruiqing <liruiqing7@163.com>
2023-02-03 14:24:03 +0800

    feat: reference npm in the example

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,10 +1,21 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import { TestDom, StyledComponent } from "canvas-pixel-react";
+
+const StyledBox = styled.div`
+  padding: 50px;
+`;
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <StyledBox>
+      <Title>My page</Title>
+      <TestDom />
+      <StyledComponent>这里</StyledComponent>
+    </StyledBox>
+  );
 }
